ca88474f8194ad34b77655adbbdde0832d06fcd6
jeongwon-Yang <ghtjd3795@naver.com>
2024-10-22 23:53:51 +0900

    Update 2024_10_23.py

--- weekly5/Jeongwon/2024_10_23.py
+++ weekly5/Jeongwon/2024_10_23.py
@@ -1,1 +1,46 @@
+# problem 1
+def solution(k, tangerine):
+    dic1 = {}
+    for i in tangerine:
+        if i not in dic1:
+            dic1[i] = 1
+        elif i in dic1:
+            dic1[i] += 1
+    
+    
+    list_dic1 = sorted(dic1.items(), key = lambda x:x[1],reverse=True)
+    
+    sum = 0
+    result = 0
+    for j in list_dic1:
+        sum += j[1]
+        result += 1
+        if sum == k:
+            return result
+    else:
+        return 1
+      
+# problem 2
+def solution(k, score):
+    lst = []
+    result = []
+    for i in score:
+        lst.append(i)
+        lst = sorted(lst, reverse = True)
+        result.append(lst[0:k][-1])
+    return result
 
+# problem 3
+
+def solution(n):
+    bin_num = bin(n)[2:]
+
+    while True:
+        n +=1
+        next_num = bin(n)[2:]
+
+        if one_count_fuc(bin_num) == one_count_fuc(next_num):
+            return n
+        
+def one_count_fuc(bin_num):
+    return bin_num.count('1')
