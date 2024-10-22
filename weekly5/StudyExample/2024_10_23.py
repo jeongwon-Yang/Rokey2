060e5bfc224bfbb81f33814fd135236363d23c51
yeonhochi <132981096+yeonhochi@users.noreply.github.com>
2024-10-22 15:36:41 +0900

    Add files via upload

---
+++ weekly5/StudyExample/2024_10_23.py
@@ -0,0 +1,10 @@
+# 문제1
+# https://school.programmers.co.kr/learn/courses/30/lessons/138476
+
+
+# 문제2
+# https://school.programmers.co.kr/learn/courses/30/lessons/138477
+
+
+# 문제3
+# https://school.programmers.co.kr/learn/courses/30/lessons/12911
