938612c8037ea8e595723e8486ab277a016dddf1
yeonhochi <132981096+yeonhochi@users.noreply.github.com>
2024-10-24 08:08:37 +0900

    Add files via upload

---
+++ weekly5/Yeonho/2024_10_24_최연호.py
@@ -0,0 +1,40 @@
+# 문제 1
+def solution(n,a,b):
+    answer = 1
+   
+    a, b = max(a, b), min(a, b)
+
+    while a % 2 != 0 or a - b != 1:
+        if a % 2 == 0:
+            a = a//2
+        else:
+            a = a//2 + 1
+
+        if b % 2 == 0:
+            b = b//2
+        else:
+            b = b//2 + 1
+        answer += 1
+    return answer
+
+# 문제2
+def solution(quiz):
+    answer = []
+    for string in quiz:
+
+        st_lst = string.split(" = ")
+        
+
+        if ' + ' in st_lst[0]:
+            num = st_lst[0].split(" + ")
+            if int(num[0]) + int(num[1]) == int(st_lst[1]):
+                answer.append("O")
+            else:
+                answer.append("X")
+        elif ' - ' in st_lst[0]:
+            num = st_lst[0].split(" - ")
+            if int(num[0]) - int(num[1]) == int(st_lst[1]):
+                answer.append("O")
+            else:
+                answer.append("X")
+    return answer
