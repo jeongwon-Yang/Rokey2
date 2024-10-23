d6bdee5892715ce39492a7b24c58639447f0aff0
jeongwon-Yang <ghtjd3795@naver.com>
2024-10-23 22:26:12 +0900

    Update 2024_10_24.py

--- weekly5/Jeongwon/2024_10_24.py
+++ weekly5/Jeongwon/2024_10_24.py
@@ -1,4 +1,32 @@
 # problem 1
+def solution(n,a,b):
+    count1 = 0
+    while a != b:
+        a = (a + 1) // 2
+        b = (b + 1) // 2
+        count1 +=1
 
+    return count1
+
+print(solution(8,4,7))
 
 # problem 2
+
+def solution(quiz):
+    lst = []
+    for i in quiz:
+        num = i.split('=')
+
+        if '+' in num[0]:
+            a, b = map(int, num[0].split(' + '))
+            result = a + b
+        elif '-' in num[0]:
+            a, b = map(int, num[0].split(' - '))
+            result = a - b
+
+        if result == int(num[1]):
+            lst.append("O")
+        else:
+            lst.append("X")
+    
+    return lst
