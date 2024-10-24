f77e41ca9f74006dce4470bcff736e362decd64c
jeongwon-Yang <ghtjd3795@naver.com>
2024-10-24 13:54:33 +0900

    Create 2024_10_25.py

---
+++ weekly5/StudyExample/2024_10_25.py
@@ -0,0 +1,8 @@
+# problem 1
+https://school.programmers.co.kr/learn/courses/30/lessons/42840
+
+# problem 2
+https://school.programmers.co.kr/learn/courses/30/lessons/12901
+
+# problem 3
+https://school.programmers.co.kr/learn/courses/30/lessons/134240
