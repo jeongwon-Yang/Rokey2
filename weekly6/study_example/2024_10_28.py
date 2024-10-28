884e68cef9b53cd4c6334fe9c1e6173be3b66500
yeonhochi <132981096+yeonhochi@users.noreply.github.com>
2024-10-28 16:40:18 +0900

    Update 2024_10_28.py

--- weekly6/study_example/2024_10_28.py
+++ weekly6/study_example/2024_10_28.py
@@ -1,1 +1,10 @@
+# 문제1
+# https://school.programmers.co.kr/learn/courses/30/lessons/12914
+
+
+# 문제2
+# https://school.programmers.co.kr/learn/courses/30/lessons/12924
+
+#문제3
+# https://school.programmers.co.kr/learn/courses/30/lessons/12945
 
