e7fc23d0d016ddf8afbb804ea664df96d6b98da5
jeongwon-Yang <ghtjd3795@naver.com>
2024-10-29 01:33:36 +0900

    Create 2024_10_29.py

---
+++ weekly6/Jeongwon/2024_10_29.py
@@ -0,0 +1,53 @@
+# problem 1
+def solution(n):
+    lst= []
+    count1 = 0
+    for i in range(n):
+        lst.append(1)
+        count1 += 1
+
+    while lst.count(1) >= 2 :
+        lst.remove(1)
+        lst.remove(1)
+        lst.append(2)
+       
+    if 1 not in lst:
+        count1 += 1
+    
+    return count1
+
+# problem 2
+def solution(n):
+    end_n = 1
+    result = 0
+    while end_n != n:
+        sum_n = 0
+        for i in range(end_n,n):
+            sum_n += i
+            
+            if sum_n == n:
+                result +=1
+                break
+            
+            elif sum_n > n:
+                break
+                
+        end_n +=1
+
+    
+    
+    return result+1
+
+# problem 3
+
+def solution(n):
+    a, b = 0, 1
+    while n >= 2:
+        a, b = b, (a+b)
+        n -= 1
+
+        print(a,b)
+    
+    return b
+
+print(solution(5))
